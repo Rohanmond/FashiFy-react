c01c1a4e4d3bb994a00eed1435f1a1bd8181c055
Rohan Mondal <mondalrohan108@gmail.com>
2022-03-22 19:13:46 +0530

    fixed cartlist

--- fashify/src/pages/Cart/CartList.jsx
+++ fashify/src/pages/Cart/CartList.jsx
@@ -1,45 +1,37 @@
-import { useData } from "../../contexts";
-import "./CartList.css";
-import CartCard from "./components/CartCard/CartCard";
+import { useData } from '../../contexts';
+import './CartList.css';
+import CartCard from './components/CartCard/CartCard';
 const CartList = () => {
   const { state } = useData();
   const cartData = [...state.cartlist];
   return (
-    <main className="cart_mngmt-main">
-      <div className="cart_mngmt-main-heading text-align-center">
+    <main className='cart_mngmt-main'>
+      <div className='cart_mngmt-main-heading text-align-center'>
         <h4>
           MY CART(
-          {cartData.reduce((acc, curr) => {
-            return acc + curr.qty;
-          }, 0)}
-          )
+          {cartData.length})
         </h4>
       </div>
-      <div className="cart_mngmt-content">
-        <div className="cart_mngmt-carts">
+      <div className='cart_mngmt-content'>
+        <div className='cart_mngmt-carts'>
           {cartData.map((el) => {
-            return (
-              <CartCard
-                el={el}
-                key={el.id}
-              />
-            );
+            return <CartCard el={el} key={el.id} />;
           })}
         </div>
         {cartData.length === 0 && <h2>Your cart is empty</h2>}
         {cartData.length !== 0 && (
-          <div className="cart_mngmt-price-details">
-            <div className="card-container cart_mngmt-price-card brd-rd-semi-sq">
-              <div className="mngmt_cart-price-header">
+          <div className='cart_mngmt-price-details'>
+            <div className='card-container cart_mngmt-price-card brd-rd-semi-sq'>
+              <div className='mngmt_cart-price-header'>
                 <h3>CART PRICE DETAILS</h3>
-                <button className="btn btn-secondary outlined-secondary brd-rd-semi-sq">
-                  <i className="fas fa-share"></i>
+                <button className='btn btn-secondary outlined-secondary brd-rd-semi-sq'>
+                  <i className='fas fa-share'></i>
                 </button>
               </div>
               {cartData.map((el) => {
-                const {_id,title,qty,price}=el;
+                const { _id, title, qty, price } = el;
                 return (
-                  <div className="mngmt_cart-price-item" key={_id}>
+                  <div className='mngmt_cart-price-item' key={_id}>
                     <p>{title}</p>
                     <p>Q:{qty}</p>
                     <p>₹{price * qty}</p>
@@ -47,17 +39,17 @@ const CartList = () => {
                 );
               })}
 
-              <div className="mngmt_cart-price-item">
-                <p className="font-wt-bold">Total price:</p>
-                <p className="font-wt-bold">
+              <div className='mngmt_cart-price-item'>
+                <p className='font-wt-bold'>Total price:</p>
+                <p className='font-wt-bold'>
                   {cartData.reduce((acc, curr) => {
                     return acc + curr.price * curr.qty;
                   }, 0)}
                 </p>
               </div>
               <a
-                className="btn btn-link-primary background-primary brd-rd-semi-sq text-align-center"
-                href="../checkout/checkout.html"
+                className='btn btn-link-primary background-primary brd-rd-semi-sq text-align-center'
+                href='../checkout/checkout.html'
               >
                 CHECKOUT
               </a>
