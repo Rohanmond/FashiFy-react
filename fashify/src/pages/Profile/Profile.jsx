1faa5cf3fe55ba8df6aff6d636dc7fcc66fc9d18
Rohan Mondal <mondalrohan108@gmail.com>
2022-04-28 06:22:13 +0530

    added address management

--- fashify/src/pages/Profile/Profile.jsx
+++ fashify/src/pages/Profile/Profile.jsx
@@ -3,29 +3,22 @@ import { NavLink, Outlet } from 'react-router-dom';
 
 export const Profile = () => {
   const toggleActive = ({ isActive }) => {
-    return isActive ? 'nav-link-active' : 'nav-link';
+    return isActive
+      ? 'profile-nav-item nav-link-active'
+      : 'profile-nav-item nav-link';
   };
   return (
     <div className='profile-outer-container'>
       <div className='profile-container'>
         <div className='profile-nav'>
-          <NavLink
-            to={'/profile/details'}
-            className={`profile-nav-item ${toggleActive}`}
-          >
+          <NavLink to={'/profile/details'} className={toggleActive}>
             Profile Information
           </NavLink>
           {/*address management and order management will be added later on*/}
-          <NavLink
-            to={'/profile/addresses'}
-            className={`profile-nav-item ${toggleActive}`}
-          >
+          <NavLink to={'/profile/addresses'} className={toggleActive}>
             Addresses
           </NavLink>
-          <NavLink
-            to={'/profile/orders'}
-            className={`profile-nav-item ${toggleActive}`}
-          >
+          <NavLink to={'/profile/orders'} className={toggleActive}>
             Order History
           </NavLink>
         </div>
--- fashify/src/pages/Profile/components/Addresses/Addresses.jsx
+++ fashify/src/pages/Profile/components/Addresses/Addresses.jsx
@@ -1,14 +1,24 @@
+import { useState } from 'react';
+import { useData } from '../../../../contexts/data-context';
 import './Addresses.css';
 import { AddressForm } from './components/AddressForm/AddressForm';
+import { AddressList } from './components/AddressList/AddressList';
 
 export const Addresses = () => {
+  const [openAddressForm, setOpenAddressForm] = useState(false);
+  const { state } = useData();
+  console.log(state);
   return (
     <div className='address-outer-container'>
       <div className='address-container'>
         <div className='header'>
           <h3>Manage your addresses</h3>
         </div>
-        <AddressForm />
+        <AddressForm
+          openAddressForm={openAddressForm}
+          setOpenAddressForm={setOpenAddressForm}
+        />
+        <AddressList />
       </div>
     </div>
   );
