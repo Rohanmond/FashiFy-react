6a9eb54d294bc45a1cf988cdcaf64976a25e7d92
Rohan Mondal <mondalrohan108@gmail.com>
2022-04-29 19:02:22 +0530

    removed unwanted commetns

--- fashify/src/Hooks/FilterHook.js
+++ fashify/src/Hooks/FilterHook.js
@@ -1,4 +1,3 @@
-import { useMemo } from 'react';
 import { useData } from '../contexts/data-context';
 import {
   categoryFilter,
@@ -26,10 +25,7 @@ export const useFilterHook = () => {
     newData = sortByPrice(newData, sortBy);
     return newData;
   };
-  // const newData = useMemo(
-  //   () => applyFilters(products, filters),
-  //   [filters, products]
-  // );
+
   const newData = applyFilters(products, filters);
   return { filteredData: newData };
 };
--- fashify/src/Hooks/Pagination.js
+++ fashify/src/Hooks/Pagination.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useMemo } from 'react';
+import { useState, useEffect } from 'react';
 
 import { useData } from '../contexts';
 
